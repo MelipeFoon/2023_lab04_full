59f38bfdd9cd54884ff752b4acb7b515dde38797
MelipeFoon <felipe.t.moon@gmail.com>
2023-10-30 12:48:31 -0300

    lab04 30_10_2023

---
+++ front/js/scripts.js
@@ -0,0 +1,20 @@
+const protocolo = "http://";
+const host = "localhost";
+const port = "3000";
+const endPoint = "/filmes";
+
+async function obterFilmes() {
+    const urlComplete = `${protocolo}${host}:${port}${endPoint}`;
+    const filmes = (await axios.get(urlComplete)).data;
+    // console.log(filmes);
+    let tabela = document.querySelector(".filmes");
+    let corpoTabela = tabela.getElementsByTagName("tbody")[0];
+    for(filme of filmes){
+        let linha = corpoTabela.insertRow(0);
+        let celulaNome = linha.insertCell(0);
+        let celulaSinopse = linha.insertCell(1);
+        celulaNome.innerHTML = filme.nome;
+        celulaSinopse.innerHTML = filme.sinopse;
+    }
+    // return filmes;
+}  
--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 let filmes = [
     {
-        id: 1,
+        // id: 1,
         nome: "Capitão America",
-        imagemUrl: "https://br.web.img3.acsta.net/pictures/14/04/11/20/05/403364.jpg",
+        sinopse: "Capitão America é um super heroi da Marvel",
+        imagemUrl: "https://th.bing.com/th/id/R.b10bd495a4eaeb6c647cddb9b5de5696?rik=aI4P9LlvVuAVgw&riu=http%3a%2f%2fliseee.l.i.pic.centerblog.net%2fo%2f1b55c854.jpg&ehk=Pev5NBVqHlDh8l26EBpfNaXNn3vhOdXAQwQTk%2fEakmo%3d&risl=&pid=ImgRaw&r=0",
     },
     {
-        id: 2,
+        // id: 2,
         nome: "Capitã Marvel",
+        sinopse: "Capitã Marvel é uma super heroina da Marvel",
         imagemUrl: "https://br.web.img3.acsta.net/pictures/19/02/04/18/35/1468867.jpg",
     },
     {
-        id: 3,
+        // id: 3,
         nome: "O Senhor dos Anéis",
+        sinopse: "O Senhor dos Anéis é um filme de fantasia",
         imagemUrl: "https://br.web.img3.acsta.net/pictures/14/03/28/20/32/395879.jpg",
-    }
+    },
+    {
+        // id: 4,
+        nome: "Harry Potter",
+        sinopse: "Harry Potter é um bruxo da Grifinória",
+        imagemUrl: "https://br.web.img3.acsta.net/pictures/14/10/30/18/07/146886.jpg",
+    },
 ]
 
 //GET https://localhost:3000/filmes
@@ -40,11 +51,12 @@ app.get('/Corno', (req, res) => {
 app.post('/filmes', (req, res) => {
     console.log(req.body.titulo);
     const filme = {
-        id: filmes.length + 1,
+        // id: filmes.length + 1,
         nome: req.body.nome,
         imagemUrl: req.body.imagemUrl
     }
     filmes.push(filme);
+    // res.json(filmes)
     res.send('Filme adicionado com sucesso');
 });
 
